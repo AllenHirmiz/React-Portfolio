a4a56c8d053389efb3e2ffee3679e586220990c9
AllenHirmiz <allenaliraqi@gmail.com>
2023-10-21 16:51:57 +1100

    Add Google Form to contact me page

--- src/components/pages/Contact.jsx
+++ src/components/pages/Contact.jsx
@@ -9,37 +9,10 @@ function Contact() {
     return (
         <div className="contact-section">
             <h2>Contact Me</h2>
-            <form>
-                <div className="form-group">
-                    <div>
-                        <label htmlFor="name">Name:</label>
-                        <input type="text" id="name" name="name" required onFocus={handleFocus} />
-                    </div>
-                    <div className="invalid-feedback">Name is required</div>
-                </div>
-
-                <div>
-                <iframe src="https://docs.google.com/forms/d/e/1FAIpQLSegQp4yfvs7P5dlxyETZHEUNZmWrIL0tDhkOViTWcIx4DD28Q/viewform?embedded=true" width="640" height="954" frameborder="0" marginheight="0" marginwidth="0">Loading…</iframe>
-                </div>
-                
-                <div className="form-group">
-                    <div>
-                        <label htmlFor="email">Email:</label>
-                        <input type="email" id="email" name="email" required onFocus={handleFocus} />
-                    </div>
-                    <div className="invalid-feedback">Please enter a valid email address</div>
-                </div>
-
-                <div className="form-group">
-                    <div>
-                        <label htmlFor="message">Message:</label>
-                        <textarea id="message" name="message" required onFocus={handleFocus}></textarea>
-                    </div>
-                    <div className="invalid-feedback">Message is required</div>
-                </div>
-
-                <button type="submit">Submit</button>
-            </form>
+            <br /><br />
+            <div>
+                <iframe src="https://docs.google.com/forms/d/e/1FAIpQLSegQp4yfvs7P5dlxyETZHEUNZmWrIL0tDhkOViTWcIx4DD28Q/viewform?embedded=true" width="80%" height="1200px" frameborder="0" marginheight="0" marginwidth="0">Loading…</iframe>
+            </div>
         </div>
     );
 }
